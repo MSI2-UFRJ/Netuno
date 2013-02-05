d4bfa2144b47f0b23ed122e5b575ef6a795d47f7
wakimjraige <wakim.jraige@gmail.com>
2013-02-05 18:36:39 -0200

    Commit inicial, apenas o Projeto com o Modelo (Nao terminado) esta presente.

---
+++ NetunoModelo/src/br/com/ufrj/msi2/netuno/modelo/Navio.java
@@ -0,0 +1,46 @@
+package br.com.ufrj.msi2.netuno.modelo;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="navio")
+public class Navio implements Serializable {
+	private static final long serialVersionUID = 5809941224721488777L;
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Integer id;
+	
+	@Column
+	private String nome;
+	
+	@OneToMany(fetch = FetchType.LAZY)
+	@JoinColumn(name="navio_id")
+	List<Carga> cargas = new ArrayList<Carga>();
+	
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+}
---
+++ NetunoModelo/src/br/com/ufrj/msi2/netuno/modelo/Pessoa.java
@@ -0,0 +1,8 @@
+package br.com.ufrj.msi2.netuno.modelo;
+
+import javax.persistence.Entity;
+
+@Entity
+public class Pessoa {
+	private Integer id;
+}
