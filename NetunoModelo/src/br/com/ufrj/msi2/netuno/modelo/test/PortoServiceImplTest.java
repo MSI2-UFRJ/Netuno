6788f1bc71aca2b4dec128601a285a3be21ef354
vittau <vittau@ig.com.br>
2013-03-15 14:33:00 -0300

    Mais testes de Porto.

--- NetunoModelo/src/br/com/ufrj/msi2/netuno/modelo/test/PortoServiceImplTest.java
+++ NetunoModelo/src/br/com/ufrj/msi2/netuno/modelo/test/PortoServiceImplTest.java
@@ -2,6 +2,8 @@ package br.com.ufrj.msi2.netuno.modelo.test;
 
 import java.util.List;
 
+import javax.persistence.EntityTransaction;
+
 import br.com.ufrj.msi2.netuno.modelo.entidades.AgenteCarga;
 import br.com.ufrj.msi2.netuno.modelo.entidades.AgenteLogistica;
 import br.com.ufrj.msi2.netuno.modelo.entidades.AgentePorto;
@@ -32,18 +34,25 @@ public class PortoServiceImplTest  extends BaseSessionBeanFixture<PortoServiceIm
 	
 	private static final Class<?>[] usedBeans = { AgenteCarga.class, AgenteLogistica.class, AgentePorto.class, AgenteRota.class, Atraque.class, Carga.class, CargaComponente.class, CargaLog.class, Conteiner.class, Contratante.class, Contrato.class, EmpresaTransporte.class, Navio.class, ParteCarga.class, Patio.class, Porto.class, Pregao.class, RepresEmpTrans.class, Requisicao.class, Slot.class, Usuario.class };
 
-	private static final CSVInitialDataSet<Porto> CSV_SET = 
+	PortoServiceImpl service;
+	
+	private static final CSVInitialDataSet<Porto> CSV_SET1 = 
             new CSVInitialDataSet<Porto>(
                     Porto.class, "porto.csv", 
                     "localizacao", "nome");
 	
+	//Não precisa estar aqui nesse teste, só para fins ilustrativos.
+	private static final CSVInitialDataSet<Patio> CSV_SET2 = 
+            new CSVInitialDataSet<Patio>(
+                    Patio.class, "patio.csv", 
+                    "id", "porto");
+	
 	public PortoServiceImplTest() {
-		super(PortoServiceImpl.class, usedBeans, CSV_SET);
+		super(PortoServiceImpl.class, usedBeans, CSV_SET1, CSV_SET2);
 	}
 	
-	//Teste para verificar se a persistência funciona corretamente.
-	public void testeObterTodos() {
-		final PortoServiceImpl service = this.getBeanToTest();
+	public void testObterTodos() {
+		service = this.getBeanToTest();
 		List<Porto> lista = service.obterTodos();
 		assertEquals("BA", (lista.get(0)).getLocalizacao());
 		assertEquals("ES", (lista.get(1)).getLocalizacao());
@@ -53,10 +62,48 @@ public class PortoServiceImplTest  extends BaseSessionBeanFixture<PortoServiceIm
 		assertEquals(5, lista.size());
 	}
 	
+	public void testObterPorId() {
+		service = this.getBeanToTest();
+		Porto porto;
+		porto = service.obterPorId(1);
+		assertEquals("RJ", porto.getLocalizacao());
+		porto = service.obterPorId(2);
+		assertEquals("SP", porto.getLocalizacao());
+		porto = service.obterPorId(3);
+		assertEquals("BA", porto.getLocalizacao());
+		porto = service.obterPorId(4);
+		assertEquals("SV", porto.getLocalizacao());
+		porto = service.obterPorId(5);
+		assertEquals("ES", porto.getLocalizacao());
+	}
+	
+	public void testeSalvar() {
+		service = this.getBeanToTest();
+		Porto porto = new Porto();
+		porto.setLocalizacao("PA");
+		porto.setNome("Pará");
+		porto.setAgentes(null);
+		porto.setAtraques(null);
+		porto.setPatios(null);
+		porto.setSlots(null);
+		
+		EntityTransaction tx = this.getEntityManager().getTransaction();
+		try {
+			tx.begin();
+			service.salvar(porto);
+			tx.commit();
+		} catch (Exception e) {
+			tx.rollback();
+			fail("Falha ao persistir.");
+		}
+		
+		List<Porto> lista = service.filtrar(porto);
+		assertEquals("PA", lista.get(0).getLocalizacao());
+		
+	}
+	
 	@Override
 	public void setUp() throws Exception {
 		super.setUp();
-		// this.tx = this.getEntityManager().getTransaction();
-		// tx.begin();
 	}
 }
